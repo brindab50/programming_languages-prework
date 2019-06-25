96542442d984a794ef9f41798b2e7776a19ba44b
brinda behal <brindab50@gmail.com>
2019-06-25 18:34:54 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,7 +7,7 @@ def reformat_languages(languages)
     name_hash.each do |language, language_data|
       new_hash[language] = language_data
       new_hash[language][:style] ||= []
-      
+        binding.pry
        new_hash[language][:style] << style
  
   end
